c44f761a2b9c84d25e8d71248fab66e763513f8e
yazanash <yazan.ash.doonaas@gmail.com>
2026-02-21 17:28:26 +0300

    rename categories property

--- Bazaar.app/Dtos/ManufacturerDto/ManufacturerResponse.cs
+++ Bazaar.app/Dtos/ManufacturerDto/ManufacturerResponse.cs
@@ -13,6 +13,6 @@ namespace Bazaar.app.Dtos.ManufacturerDto
 
         public int Id => Manufacturer.Id;
         public string Name => Manufacturer.Name;
-        public List<Category> SupportedCategries => Manufacturer.SupportedCategories;
+        public List<Category> SupportedCategories => Manufacturer.SupportedCategories;
     }
 }
