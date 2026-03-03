fb2a772def79d70c369a06ee6b4bb2f132348ef0
yazanash <yazan.ash.doonaas@gmail.com>
2026-03-03 12:43:40 +0300

    add ad banner service to host builder

--- Bazaar.app/HostBuilders/DataServiceExtension.cs
+++ Bazaar.app/HostBuilders/DataServiceExtension.cs
@@ -20,6 +20,7 @@ namespace Bazaar.app.HostBuilders
             services.AddScoped<IUserWalletService, UserWalletDataService>();
             services.AddScoped<IPackageDataService, PackageDataService>();
             services.AddScoped<IDataService<AdBanners>, AdBannersDataService>();
+            services.AddScoped<IAdBannerAdminService, AdBannersDataService>();
             services.AddScoped<IStatsDataService, StatsDataService>();
             services.AddScoped<IPaymentGatewayDataService, PaymentGatewayDataService>();
             services.AddScoped<IUserStateService, UserStateService>();
